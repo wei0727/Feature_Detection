7a3a6a47382579114b60005d8f5064fe74af34b0
wei0727 <phonneog@gmail.com>
2014-05-09 22:23:27 +0800

    setting threshold..not done

--- Main.cpp
+++ Main.cpp
@@ -56,19 +56,48 @@ int inliers(vector<Point2d> &pts_obj, vector<Point2d> &pts_target, vector<vector
 	transform_obj.reserve(pts_obj.size()) ;
 	perspectiveTransform(pts_obj, transform_obj, transform_mat) ;
 	int inliers = 0 ;
+	int total_num = 0 ;
 	total_error = 0 ;
 	for(int i=0; i<matches.size(); i++){
 		for(int k=0; k<matches[i].size(); k++){
 			Point2d dif = transform_obj[matches[i][k].queryIdx] - pts_target[matches[i][k].trainIdx] ;
 			double d = sqrt(dif.x*dif.x + dif.y*dif.y) ;
 			total_error += d ;
+			total_num ++ ;
 			if(d < threshold){
 				inliers += 1 ;
 				break ;
 			}
 		}
 	}
+	total_error /= total_num ;
 	return inliers ;
+}
+
+bool niceHomography(Mat &h){
+	double *r0 = h.ptr<double>(0) ;
+	double *r1 = h.ptr<double>(1) ;
+	double *r2 = h.ptr<double>(2) ;
+
+	double det = r0[0]*r1[1] - r1[0]*r0[1] ;
+	if(det < 0)
+		return false ;
+
+	double n1 = sqrt(r0[0]*r0[0] + r1[0]*r1[0]) ;
+	cout << "n1 " << n1 << endl ;
+	//if(n1>4 || n1<0.1)
+	//	return false ;
+
+	double n2 = sqrt(r0[1]*r0[1] + r1[1]*r1[1]) ;
+	cout << "n2 " << n2 << endl ;
+	//if(n2>4 || n2<0.1)
+	//	return false ;
+
+	double n3 = sqrt(r2[0]*r2[0] + r2[1]*r2[1]) ;
+	cout << "n3 " << n3 << endl ;
+	//if(n3>4 || n3<0.1)
+	//	return false ;
+	return true ;
 }
 
 void normalized_dlt(vector<Point2d> &obj, vector<Point2d> &target, Mat &transform_mat){
@@ -124,11 +153,13 @@ void normalized_dlt(vector<Point2d> &obj, vector<Point2d> &target, Mat &transfor
 void knn_match(int k, Mat &descriptor_obj, Mat &descriptor_target, vector<vector<DMatch>> &matches, vector<vector<DMatch>> &good_matches){
 	double mind = DBL_MAX ;
 	double maxd = 0 ;
+	double mean = 0 ;
 	for(int i=0; i<descriptor_obj.rows; i++){
 		for(int k=0; k<descriptor_target.rows; k++){
 			double distance = norm(descriptor_obj.row(i), descriptor_target.row(k)) ;
 			mind = min(mind, distance) ;
 			maxd = max(maxd, distance) ;
+			mean += distance ;
 			if(matches[i].size()==0)
 				matches[i].push_back(DMatch(i, k, distance)) ;
 			else{
@@ -139,6 +170,10 @@ void knn_match(int k, Mat &descriptor_obj, Mat &descriptor_target, vector<vector
 			}
 		}
 	}
+	mean = mean/(descriptor_obj.rows*descriptor_target.rows) ;
+	cout << "min " << mind << endl ;
+	cout << "max " << maxd << endl ;
+	cout << "mean " << mean << endl ;
 	mind *= 3 ;
 	if(mind == 0){
 		mind = maxd/3 ;
@@ -154,18 +189,23 @@ void knn_match(int k, Mat &descriptor_obj, Mat &descriptor_target, vector<vector
 		if(!tmp.empty())
 			good_matches.push_back(tmp) ;
 	}
-	good_matches = matches ;
+	//good_matches = matches ;
 }
 
-void ransac(vector<KeyPoint> &kps_obj, vector<KeyPoint> &kps_target, vector<Point2d> &pts_obj, vector<Point2d> &pts_target, vector<vector<DMatch>> &good_matches, Mat &homography){
+double ransac(vector<KeyPoint> &kps_obj, vector<KeyPoint> &kps_target, vector<Point2d> &pts_obj, vector<Point2d> &pts_target, vector<vector<DMatch>> &good_matches, Mat &homography){
 	srand(time(NULL)) ;
 	const int max_round = 80000 ;
 	int count_round = 0 ;
 	vector<Mat> candidates ;
 	vector<int> candidates_inliers ;
 	int max_inliers=0, max_index=-1 ;
-	while(count_round < max_round){
+	double rate=0.6 ;
+	while(true){
 		count_round++ ;
+		if(count_round>200){
+			rate *= 0.9 ;
+			count_round = 0 ;
+		}
 		int rndpts[4] ;
 		for(int i=0; i<4; i++){
 			while(true){
@@ -218,23 +258,25 @@ void ransac(vector<KeyPoint> &kps_obj, vector<KeyPoint> &kps_target, vector<Poin
 						//normalized_dlt(tmp_obj, tmp_target, transform_mat) ;
 						//////////////////////////////////
 						double error ;
-						int num_inliers = inliers(pts_obj, pts_target, good_matches, transform_mat, 60.0, error) ;
-						if(num_inliers > good_matches.size()*0.6){
-							//homography = transform_mat.clone() ;
-							candidates.push_back(transform_mat.clone()) ;
-							candidates_inliers.push_back(num_inliers) ;
-							if(num_inliers > max_inliers){
-								max_inliers = num_inliers ;
-								max_index = candidates.size()-1 ;
-							}
+						int num_inliers = inliers(pts_obj, pts_target, good_matches, transform_mat, 20.0, error) ;
+						if(num_inliers > good_matches.size()*rate){
+							homography = transform_mat.clone() ;
+							cout << "inliers " << num_inliers << endl ;
+							cout << "error " << error << endl ;
+							return error ;
+
+							//candidates.push_back(transform_mat.clone()) ;
+							//candidates_inliers.push_back(num_inliers) ;
+							//if(num_inliers > max_inliers){
+							//	max_inliers = num_inliers ;
+							//	max_index = candidates.size()-1 ;
+							//}
 						}
 					}
 				}
 			}
 		}
 	}
-	cout << good_matches.size() << "\t" << max_inliers << endl ;
-	homography = candidates[max_index] ;
 }
 
 void ransac_debug(vector<KeyPoint> &kps_obj, vector<KeyPoint> &kps_target, vector<Point2d> &pts_obj, vector<Point2d> &pts_target, vector<vector<DMatch>> &good_matches, Mat &homography, Mat img, Mat target){
@@ -353,9 +395,9 @@ void ransac_debug(vector<KeyPoint> &kps_obj, vector<KeyPoint> &kps_target, vecto
 	}
 }
 
-void featureDetection(string obj, string target){
-	Mat img_obj = imread("object/c0.jpg") ;
-	Mat img_target = imread("target/target1.jpg") ;
+void featureDetection(string obj, string target, double &error, Mat &result){
+	Mat img_obj = imread(obj) ;
+	Mat img_target = imread(target) ;
 	SiftFeatureDetector detector(0.05, 5.0) ;
 	vector<KeyPoint> kps_obj, kps_target ;
 	vector<Point2d> pts_obj, pts_target ;
@@ -371,33 +413,56 @@ void featureDetection(string obj, string target){
 	extractor.compute(img_target, kps_target, descriptor_target) ;
 
 	vector<vector<DMatch>> matches(pts_obj.size()), good_matches ;
-	knn_match(100, descriptor_obj, descriptor_target, matches, good_matches) ;
-	cout << "matches: " << matches.size() << endl ;
+	knn_match(4, descriptor_obj, descriptor_target, matches, good_matches) ;
 	cout << "good matches: " << good_matches.size() << endl ; ;
 
 	Mat homography ;
-	ransac_debug(kps_obj, kps_target, pts_obj, pts_target, good_matches, homography, img_obj, img_target) ;
-	//ransac(kps_obj, kps_target, pts_obj, pts_target, good_matches, homography) ;
-
+	//ransac_debug(kps_obj, kps_target, pts_obj, pts_target, good_matches, homography, img_obj, img_target) ;
+	error = ransac(kps_obj, kps_target, pts_obj, pts_target, good_matches, homography) ;
+	cout << "niceHomography " << niceHomography(homography) << endl ;
+	if(!niceHomography(homography))
+		error = DBL_MAX ;
 	//draw box
-	//vector<Point2d> obj_corner(4, Point2d(0, 0)), target_corner ;
-	//obj_corner[1] = Point2d(img_obj.cols, 0) ;
-	//obj_corner[2] = Point2d(img_obj.cols, img_obj.rows) ;
-	//obj_corner[3] = Point2d(0, img_obj.rows) ;
-	//perspectiveTransform(obj_corner, target_corner, homography) ;
-	//Mat tmp_img = img_target.clone() ;
-	//for(int a=0; a<4; a++)
-	//	circle(tmp_img, target_corner[a], 5, Scalar(255, 255, 0), 2) ;
-	//line(tmp_img, target_corner[0], target_corner[1], Scalar(0, 255, 0)) ;
-	//line(tmp_img, target_corner[1], target_corner[2], Scalar(0, 255, 0)) ;
-	//line(tmp_img, target_corner[2], target_corner[3], Scalar(0, 255, 0)) ;
-	//line(tmp_img, target_corner[3], target_corner[0], Scalar(0, 255, 0)) ;
-	//imshow("tmp", tmp_img) ;
-	//cvWaitKey(0) ;
-	//cvDestroyAllWindows() ;
+	vector<Point2d> obj_corner(4, Point2d(0, 0)), target_corner ;
+	obj_corner[1] = Point2d(img_obj.cols, 0) ;
+	obj_corner[2] = Point2d(img_obj.cols, img_obj.rows) ;
+	obj_corner[3] = Point2d(0, img_obj.rows) ;
+	perspectiveTransform(obj_corner, target_corner, homography) ;
+	result = img_target.clone() ;
+	for(int a=0; a<4; a++)
+		circle(result, target_corner[a], 5, Scalar(255, 255, 0), 2) ;
+	line(result, target_corner[0], target_corner[1], Scalar(0, 255, 0)) ;
+	line(result, target_corner[1], target_corner[2], Scalar(0, 255, 0)) ;
+	line(result, target_corner[2], target_corner[3], Scalar(0, 255, 0)) ;
+	line(result, target_corner[3], target_corner[0], Scalar(0, 255, 0)) ;
+	
 }
 
 int main(){
-	featureDetection("a1.jpg", "target1.jpg") ;
+	Mat result ;
+	double error = DBL_MAX ;
+	vector<vector<string>> objs;
+	string obj_a[] = {"object/a1.jpg", "object/a2.jpg", "object/a4.jpg", "object/a5.jpg", "object/a6.jpg"} ;
+	string obj_b[] = {"object/b1.jpg", "object/b2.jpg", "object/b3.jpg"} ;
+	string obj_c[] = {"object/c1.jpg", "object/c2.jpg", "object/c3.jpg", "object/c4.jpg"} ;
+	objs.push_back(vector<string>(obj_a, obj_a + 5)) ;
+	objs.push_back(vector<string>(obj_b, obj_b + 3)) ;
+	objs.push_back(vector<string>(obj_c, obj_c + 4)) ;
+	Mat tmp_mat ; 
+	double tmp_error ;
+	for(int i=0; i<objs[2].size(); i++){
+		featureDetection(objs[2][i], "target/target1.jpg", tmp_error, tmp_mat) ;
+		if(tmp_error < error){
+			error = tmp_error ;
+			result = tmp_mat.clone() ;
+		}
+		string name = "a" ;
+		name[0] += i ;
+		imshow(name, tmp_mat) ;
+		cout << i << "\t" << tmp_error << endl ;
+		cout << "------------------------" << endl ;
+	}
+	imshow("result", result) ;
+	waitKey(0) ;
 	return 0 ;
 }
